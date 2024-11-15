fc839fdbc8450e611e0ef3c6531ad8712b53024d
Borges Mellerlin <bmellerlin@suppliers.tenaris.com>
2024-11-15 16:01:34 -0300

    pagina de heroes

--- src/app/heroes/pages/heroes-page/heroes-page.component.ts
+++ src/app/heroes/pages/heroes-page/heroes-page.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/hero.service';
+import { ActivatedRoute, Route, Router, RouterLink } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-heroes-page',
   templateUrl: './heroes-page.component.html',
   styles: ``
 })
-export class HeroesPageComponent {
+export class HeroesPageComponent implements OnInit{
+  
+  public hero?: Hero;
+
+  constructor(private heroesServices:HeroesService, private activeRoute:ActivatedRoute,
+              private router:Router){}
+  
+  ngOnInit(): void {
+    this.activeRoute.params
+    .pipe(
+      switchMap( ({ id }) => this.heroesServices.getHeroById( id )),
+    )
+    .subscribe( hero => {
+
+      if ( !hero ) return this.router.navigate([ '/heroes/list' ]);
+
+      this.hero = hero;
+      return;
+    })
+  }
+
+  goBack():void {
+    this.router.navigateByUrl('heroes/list')
+  }
 
 }
--- src/app/heroes/pipes/hero-image.pipe.ts
+++ src/app/heroes/pipes/hero-image.pipe.ts
@@ -7,7 +7,7 @@ import { Hero } from '../interfaces/hero.interface';
 export class HeroImagePipe implements PipeTransform {
 
   transform(hero:Hero): string {
-    console.log('El id ',hero.id);
+   
     if ( !hero.id && !hero.alt_img ) {
       return 'assets/no-image.png';
     }
